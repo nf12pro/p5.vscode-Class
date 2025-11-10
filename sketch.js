4f73ee68c04d8958613988685e47ed6b1603e098
nf12pro <rayofaik@gmail.com>
2025-11-10 16:51:38 +0100

    Day 1 Progress

--- sketch.js
+++ sketch.js
@@ -1,22 +1,11 @@
+//region Setup
 function setup() {
   createCanvas(400, 400);
-  noStroke();
 }
+//endregion
 
+//region Draw
 function draw() {
-  // clear each frame
   background(220);
-
-  // static circle for reference
-  fill(150);
-  circle(200, 200, 100);
-
-  // circle that follows the mouse; change fill when mouse is pressed
-  if (mouseIsPressed) {
-    fill(255, 0, 0); // red while pressed
-  } else {
-    fill(255); // white when not pressed
-  }
-  // draw the circle at the current mouse position every frame
-  circle(mouseX, mouseY, 100);
 }
+//#endregion
