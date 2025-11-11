a459e77ef5db1610a990b011994d5f023a3f931b
nf12pro <rayofaik@gmail.com>
2025-11-11 10:42:52 +0100

    Early Circle

--- sketch.js
+++ sketch.js
@@ -1,11 +1,21 @@
+var react_time = false;
 //region Setup
 function setup() {
   createCanvas(400, 400);
+  noStroke();
+  fill(0, 122, 255); // blue
 }
 //endregion
 
-//region Draw
+//region Draw 
 function draw() {
   background(220);
-}
+  if (mouseIsPressed) and (not react_time) {
+    fill(255, 0, 0); // red when pressed
+  } else {
+    fill(0, 122, 255); // blue otherwise
+  }
+  circle(200, 200, 50);
+
+} 
 //#endregion
